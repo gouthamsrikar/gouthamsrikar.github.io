63aaeb506481063b410d9018717b476fc4f84561
goutham srikar <srikarsam2000@gmail.com>
2022-12-05 17:42:00 +0530

    navbar shadow hover effect

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import About from './components/About';
 import Main from './components/Main';
 import Skills from './components/Skills';
 import Experience from './components/Experience';
 import Contact from './components/Contact';
 import NavBar from './components/NavBar';
-import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, { Swiper, SwiperSlide } from "swiper/react";
 import { useState, useEffect } from 'react';
 // Import Swiper styles
 import "swiper/css";
@@ -13,11 +13,17 @@ import "swiper/css";
 // import required modules
 import { Mousewheel, Pagination } from "swiper";
 function App() {
+
+  const sliderRef = useRef<SwiperCore.SwiperRef>(null);
+
+  useEffect(() => { }, [sliderRef])
+
   return (
     <div className="App">
-       <div className='h-full w-full  bg-white '>
-        <NavBar />
+      <div className='h-full w-full  bg-white '>
+        <NavBar swiperRef={sliderRef.current!} />
         <Swiper
+          ref={sliderRef}
           direction={"vertical"}
           // slidesPerView={1}
           spaceBetween={0}
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,8 +2,13 @@
 import React, { useState } from 'react'
 import { AiOutlineClose, AiOutlineMail, AiOutlineMenu } from 'react-icons/ai'
 import { FaGithub, FaLinkedin, FaTwitter, FaInstagram } from 'react-icons/fa'
+import SwiperCore, { Swiper, SwiperSlide } from "swiper/react";
 
-const NavBar = () => {
+type NavBarProps = {
+    swiperRef: SwiperCore.SwiperRef
+}
+
+const NavBar = (props: NavBarProps) => {
     const [nav, setNav] = useState(false);
 
     const handleNav = () => {
@@ -11,28 +16,26 @@ const NavBar = () => {
     }
 
     return (
-        <div className='fixed w-full h-20 shadow-xl z-[100] items-center bg-white'>
+        <div className='fixed w-full h-20 transition-shadow duration-1000 hover:shadow-xl z-[100] items-center bg-white'>
             <div className='h-full'>
 
                 <ul className='hidden md:flex justify-center px-5 py-7'>
-                    <a href='/' >
-                        <li className='ml-10 text-sm uppercase hover:border-b'>Home</li>
-                    </a>
-                    <a href='/#about'>
-                        <li className='ml-10 text-sm uppercase hover:border-b'>About</li>
-                    </a>
-                    <a href='/#skills'>
-                        <li className='ml-10 text-sm uppercase hover:border-b'>Skills</li>
-                    </a>
-                    <a href='/#experience'>
-                        <li className='ml-10 text-sm uppercase hover:border-b'>experience</li>
-                    </a>
-                    {/* <a href='/'>
-                        <li className='ml-10 text-sm uppercase hover:border-b'>Projects</li>
-                    </a> */}
-                    <a href='/#contact'>
-                        <li className='ml-10 text-sm uppercase hover:border-b'>contact</li>
-                    </a>
+
+                    <div onClick={() => {
+                        props.swiperRef.swiper.slideTo(0)
+                    }} className='ml-10 text-sm uppercase hover:border-b cursor-pointer'>Home</div>
+                    <div onClick={() => {
+                        props.swiperRef.swiper.slideTo(1)
+                    }} className='ml-10 text-sm uppercase hover:border-b cursor-pointer'>About</div>
+                    <div onClick={() => {
+                        props.swiperRef.swiper.slideTo(2)
+                    }} className='ml-10 text-sm uppercase hover:border-b cursor-pointer'>Skills</div>
+                    <div onClick={() => {
+                        props.swiperRef.swiper.slideTo(3)
+                    }} className='ml-10 text-sm uppercase hover:border-b cursor-pointer'>experience</div>
+                    <div onClick={() => {
+                        props.swiperRef.swiper.slideTo(4)
+                    }} className='ml-10 text-sm uppercase hover:border-b cursor-pointer'>contact</div>
                 </ul>
 
 
@@ -45,7 +48,7 @@ const NavBar = () => {
                 <div className={nav ? "fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-white p-6 ease-in duration-500" : "fixed left-[-100%] top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-white p-6 ease-in duration-500"}>
                     <div className='flex w-full items-center justify-end'>
                         <div onClick={handleNav} className='rounded-full shadow-lg hover:rotate-90  transition duration-1000 shadow-gray-400 p-3 cursor-pointer'>
-                            <AiOutlineClose  />
+                            <AiOutlineClose />
                         </div>
                     </div>
                     <div className='border-b border-gray-300 my-4'>
